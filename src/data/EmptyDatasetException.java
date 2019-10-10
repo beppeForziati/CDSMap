f9784b8affbe352567e8088069d821380bbd9718
beppeForziati <giuseppeforziati96@gmail.com>
2019-10-10 09:55:35 +0200

    cartelle eliminate

--- src/data/EmptyDatasetException.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-@SuppressWarnings("serial")
-public class EmptyDatasetException extends Exception {
-
-	EmptyDatasetException() {
-		super("no insert data!");
-	}
-}
--- src/mining/ClusteringRadiusException.java
+++
@@ -1,9 +0,0 @@
-package mining;
-
-@SuppressWarnings("serial")
-public class ClusteringRadiusException extends Exception {
-
-	ClusteringRadiusException() {
-		super("14 tuples in one cluster!");
-	}
-}
